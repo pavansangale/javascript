1c6ee90ad395528cac388ba395d8777467a5f591
pavansangale <44120741+pavansangale@users.noreply.github.com>
2024-03-18 12:13:01 +0000

    data types

---
+++ 01-basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict" // treat all JS code as newer version
+
+// alert( 3 + 3 )  // not allowed as we are using nodejs, not browser
+
+let name = "pavan"
+let age = 22
+let isLoggedIn = false
+
+/*
+Data types :
+
+number => 2 to power 53
+bigint 
+string => ""
+boolean => true/false
+null => standalone value
+undefined => 
+symbol => unique
+
+object
+*/
+
+console.log(typeof null)  // output : object
+console.log(typeof undefined) // output : undefined
