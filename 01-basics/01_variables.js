ee965ae1ab24e9d9c388b00a49489f38bbb525f8
pavansangale <44120741+pavansangale@users.noreply.github.com>
2024-03-18 12:14:42 +0000

    varaibles

---
+++ 01-basics/01_variables.js
@@ -0,0 +1,16 @@
+ const accountID = 12345
+ let accountEmail = "pavan@google.com"
+ var accountPassword = "10213"
+ accountCity = "pune"
+
+ let accountState
+ // accountID = 2 // not allowed
+
+ accountEmail = "pp@gg.com"
+ accountPassword = "1454"
+ accountCity = "Mumbai"
+
+ console.log(accountID)
+
+ console.table([accountID, accountEmail, accountPassword, accountCity, accountState])
+
