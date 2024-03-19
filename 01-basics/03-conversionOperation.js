afefd81ee6e56656bea855df2239b4dfeb74db46
pavansangale <44120741+pavansangale@users.noreply.github.com>
2024-03-19 07:00:06 +0000

    ops

---
+++ 01-basics/03-conversionOperation.js
@@ -0,0 +1,16 @@
+let score = "abc33"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+let str1 = "hello"
+let str2 = " Pavan"
+
+let str3 = str1 + str2
+
+console.log(str3)
